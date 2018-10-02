fe768b7c2927ce850e83393a4aad406db44e2737
SoIidarity <christopher.maree@gmail.com>
2018-10-02 11:30:06 +0200

    got popups working

--- Frontend/application/pages/MainMap.vue
+++ Frontend/application/pages/MainMap.vue
@@ -10,9 +10,13 @@
       
       <div id="map-wrap" style="height: 500px; width: 1000px">
         <no-ssr>
-          <l-map :zoom=13 :center="[47.413220, -1.219482]">
+          <l-map :zoom=13 :center="center">
             <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
             <l-marker :lat-lng="[47.413220, -1.219482]"></l-marker>
+              <l-geo-json
+                :geojson="bicycleAndCampus.geojson"
+                :options="bicycleAndCampus.options"/>
+
           </l-map>
         </no-ssr>
       </div>
@@ -22,19 +26,59 @@
 </template>
 
 <script>
+import Vue from 'vue';
+
+import { default as data } from '../assets/geojson/sample-geojson.js'
+
+import PopupContent from '~/components/MapPopup'
+
+function onEachFeature (feature, layer) {
+  let PopupCont = Vue.extend(PopupContent);
+  let popup = new PopupCont({ propsData: { type: feature.geometry.type, text: feature.properties.popupContent } });
+  layer.bindPopup(popup.$mount().$el);
+
+
+
+  console.log("ON EACH")
+  // console.log(content)
+  // console.log(feature)
+  // console.log(layer)
+}
 
 export default {
-  components: {
-  
-  },
+  components: {},
 
   data() {
     return {
-      
+      zoom: 13,
+      center: [39.74739, -105],
+      url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+      attribution:
+        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      marker: [47.41322, -1.219482],
+      currentZoom: 13,
+      showParagraph: false,
+      bicycleAndCampus: {
+        geojson: [data.campus],
+        options: {
+          style: function(feature) {
+            return feature.properties && feature.properties.style
+          },
+          onEachFeature: onEachFeature,
+        }
+      }
     }
   },
   methods: {
-    
+    zoomUpdate(zoom) {
+      this.currentZoom = zoom
+    },
+    centerUpdate(center) {
+      this.currentCenter = center
+    },
+    showLongTooltip() {
+      this.showParagraph = !this.showParagraph
+    }
   },
 
   mounted() {
@@ -44,5 +88,4 @@ export default {
 </script>
 
 <style>
-
 </style>
