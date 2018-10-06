be43774e8a1491c772748c2d67789740524591f9
Christopher Maree <christopher.maree@gmail.com>
2018-10-06 18:20:32 +0200

    Implemented docker container

--- Backend/swagger_server/__main__.py
+++ Backend/swagger_server/__main__.py
@@ -1,16 +1,13 @@
+
 #!/usr/bin/env python3
 
 import connexion
+from flask_cors import CORS
 
-from swagger_server import encoder
-
-
-def main():
-    app = connexion.App(__name__, specification_dir='./swagger/')
-    app.app.json_encoder = encoder.JSONEncoder
-    app.add_api('swagger.yaml', arguments={'title': 'Energy Consumption Optimization Mapagement Platform'})
-    app.run(port=8080)
-
+app = connexion.FlaskApp(__name__, specification_dir='./swagger/')
+app.add_api('swagger.yaml', arguments={
+            'title': 'BigchainDB API'})
+CORS(app.app)
 
 if __name__ == '__main__':
-    main()
+    app.run()
