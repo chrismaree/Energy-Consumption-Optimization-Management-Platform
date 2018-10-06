6a08d94919ce344b5929ef0a06b711301047e33d
Christopher Maree <christopher.maree@gmail.com>
2018-10-06 21:49:57 +0200

    updated API implemenatation

---
+++ Frontend/utils/databaseAccessor.js
@@ -0,0 +1,22 @@
+/* eslint-disable */
+import axios from 'axios';
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+export default class DatabaseAccessor {
+    constructor() {
+        this.BASE_URL = 'http://0.0.0.0:8080/v2';
+    }
+    // retrieve all students belonging to one university
+    getGeoJson(university_id) {
+        university_id = 0
+        let url = `${this.BASE_URL}/geojson/${university_id}`
+        return axios.get(url, {
+            // headers: {
+            //     Authorization: "Bearer " + access_token
+            // }
+        }).then(response => {
+            console.log(response.data)
+            return response.data;
+        });
+    }
+}
