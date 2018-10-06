89895a2104ce19f29fc240d932c7c0f00eaa6fd4
Christopher Maree <christopher.maree@gmail.com>
2018-10-06 21:53:25 +0200

    Added campus controller

---
+++ Backend/swagger_server/controllers/campus_controller.py
@@ -0,0 +1,16 @@
+import connexion
+import six
+
+from swagger_server.models.campus import Campus  # noqa: E501
+from swagger_server import util
+
+
+def get_campus_information():  # noqa: E501
+    """Return information on all campus
+
+    Key information for each campus # noqa: E501
+
+
+    :rtype: Campus
+    """
+    return 'do some magic!'
