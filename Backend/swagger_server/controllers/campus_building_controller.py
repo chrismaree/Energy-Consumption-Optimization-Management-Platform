1e54148de4c95d7b05aabd3d9293b660e6c775d4
Christopher Maree <christopher.maree@gmail.com>
2018-10-07 12:50:21 +0200

    Updated Swagger Python API

--- Backend/swagger_server/controllers/campus_building_controller.py
+++ Backend/swagger_server/controllers/campus_building_controller.py
@@ -3,8 +3,22 @@ import six
 
 from .global_vars import ECOMP_DB
 
+from swagger_server.models.building_information import BuildingInformation  # noqa: E501
 from swagger_server.models.campus import Campus  # noqa: E501
 from swagger_server import util
+
+
+def get_building_info_by_id(buildingId):  # noqa: E501
+    """Find building by id
+
+    Returns a single campus geojson # noqa: E501
+
+    :param buildingId: ID of campus to return geojson for
+    :type buildingId: int
+
+    :rtype: BuildingInformation
+    """
+    return 'do some magic!'
 
 
 def get_campus_information():  # noqa: E501
@@ -15,6 +29,7 @@ def get_campus_information():  # noqa: E501
     :rtype: Campus
     """
     return _stringify_object_id(ECOMP_DB.campusInfo.find({}))
+
 
 def _stringify_object_id(result):
     stringified_result = []
